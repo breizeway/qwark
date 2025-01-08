bb28177b20ac2a51f07ddb58badc7722cfcda029
Tannor Breitigam <70126993+breizeway@users.noreply.github.com>
2025-01-07 16:37:17 -0800

    theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // https://v4-test.tailwindcss.com/docs/installation/framework-guides/astro
+  vite: { plugins: [tailwindcss()] },
+});
