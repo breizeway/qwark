2d8182b5f29b0ea51a565f7680f886cfea0b11ee
Tannor Breitigam <70126993+breizeway@users.noreply.github.com>
2025-01-11 11:07:46 -0800

    react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   // https://v4-test.tailwindcss.com/docs/installation/framework-guides/astro
   output: "server",
+
   devToolbar: { enabled: false },
   vite: { plugins: [tailwindcss()] },
+  integrations: [react()],
 });
---
+++ src/components/ common/timer.tsx
@@ -0,0 +1,11 @@
+import { type Duration } from "date-fns";
+
+interface TimerProps {
+  duration: Duration;
+}
+
+export const Timer: React.FC<TimerProps> = ({}) => {
+  // const [timeLeft, setTimeLeft] = useState(duration);
+
+  return <>Timer</>;
+};
